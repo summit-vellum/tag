7e7b897ef0233ab0494fe3dbd7ed2a945ecfe062
Gien Mangaliman <Gien.Mangaliman@summitmedia.com.ph>
2020-03-05 18:05:32 +0800

    tags table structure

--- src/database/migrations/2020_03_05_091334_create_tag_table.php
+++ src/database/migrations/2020_03_05_091334_create_tag_table.php
@@ -15,6 +15,19 @@ class CreateTagTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('slug', 255);
+            $table->text('meta_title')->nullable();
+            $table->text('meta_keyword')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->integer('count');
+            $table->tinyInteger('status')->comment('1=active; 0=inactive')->default(1);
+            $table->tinyInteger('is_visible')->comment('1=visible; 0=invisible');
+            $table->tinyInteger('type')->default(1);
+            $table->string('definition', 255)->nullable();
+            $table->integer('central_id')->nullable();
+            $table->tinyInteger('nsfw')->default(0);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
